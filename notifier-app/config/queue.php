6ad6bb37217b1cd56d9a7b756c64d6ad3f2a35ff
Albert <haralb01@gmail.com>
2026-05-19 03:05:18 +0400

    Config fixses

--- notifier-app/config/queue.php
+++ notifier-app/config/queue.php
@@ -98,6 +98,7 @@ return [
                     'port' => env('RABBITMQ_PORT'),
                     'user' => env('RABBITMQ_USER'),
                     'password' => env('RABBITMQ_PASSWORD'),
+                    'vhost' => env('RABBITMQ_VHOST', '/'),
                 ],
             ],
         ],
