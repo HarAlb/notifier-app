eda853efb31acb379d49ddf1392415074c3de8cf
Albert <haralb01@gmail.com>
2026-05-19 11:26:44 +0400

    FIX: Install composer in container

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+until nc -z notifier_app_rabbitmq 5672; do
+  echo "Waiting for RabbitMQ..."
+  sleep 2
+done
+
+sleep 5
+
 chown -R www-data:www-data /var/www/notifier-app/storage
 chown -R www-data:www-data /var/www/notifier-app/bootstrap/cache
 
